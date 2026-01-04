331a581b840b64668b9a003adeb660a109821675
wadoodSaleh <wsaleh1@asu.edu>
2026-01-04 19:28:32 +0200

    fix that did not work

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root', // Assuming default root user for local dev 
-  password: '', 
+  password: '1234', 
   database: 'egg_db',
   waitForConnections: true,
   connectionLimit: 10,
